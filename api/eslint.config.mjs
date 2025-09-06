73f3201438abc31e3d4bacfe153b94fa16336341
jonathan-4a <jonathayalew@gmail.com>
2025-09-05 21:02:25 -0300

    Add eslint config

---
+++ api/eslint.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs', 'docs/**/*.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      // Prettier rule with semicolons enforced
+      'prettier/prettier': ['error', { endOfLine: 'auto', semi: true }],
+    },
+  },
+)
+
